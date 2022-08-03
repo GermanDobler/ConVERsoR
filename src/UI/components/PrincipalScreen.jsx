5d43a9a75feca459f1fb35718dc4c9ace623dee2
GermanDobler <german.crack@hotmail.com>
2022-08-03 15:02:10 -0300

    New: PrincipalScreen *Se añadio PrincipalScreen

---
+++ src/UI/components/PrincipalScreen.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import "./styles/PrincipalScreen.css";
+export default function PrincipalScreen(){
+return (
+    <Fragment>
+        <div>
+            dasd
+        </div>
+    </Fragment>
+);
+}
