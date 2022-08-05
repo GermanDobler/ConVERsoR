20ce5b799e5a0db517356e863f8f5c7359bd63fd
GermanDobler <german.crack@hotmail.com>
2022-08-05 15:25:22 -0300

    Fix: Un poco de todo

--- src/UI/components/Header.jsx
+++ src/UI/components/Header.jsx
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
-import "./styles/Header.css";
-export default function Header(){
-    return(
+import "./styles/Glassmorphism.css";
+export default function Header() {
+    return (
         <Fragment>
-            <div className="container-header">
-                
+            <div className="contenedor">
+                <div className="contenedor-header Glassmorphism">
+                    <img src={require('./CONVERSOR LOGO.png')} className="img" height={150} width={700} alt="CONVERSOR LOGO" />
+                </div>
             </div>
         </Fragment>
     );
--- src/UI/components/PrincipalScreen.jsx
+++ src/UI/components/PrincipalScreen.jsx
@@ -1,14 +1,12 @@
 import { Fragment } from "react";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import "./styles/PrincipalScreen.css";
 import Header from "./Header";
+import Historial from "./Historial";
 export default function PrincipalScreen(){
 return (
     <Fragment>
-        <div>
-            <Header></Header>
-        </div>
+        {/* <Header></Header> */}
+        <Historial></Historial>
     </Fragment>
 );
 }
