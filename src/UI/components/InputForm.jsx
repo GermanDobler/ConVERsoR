78e8fdac6995133625edf983bd3e6148e09e6b37
Daniel Chavez <daniel.chavez.epet20@gmail.com>
2022-08-06 15:24:02 -0300

    New: Agrego un input y un boton de entrada Ahora al seleccionar los valores de entrada y enviarlos se imprimen por consola.

--- src/UI/components/InputForm.jsx
+++ src/UI/components/InputForm.jsx
@@ -1,13 +1,24 @@
-import { FormControl, InputLabel, Select, MenuItem } from "@mui/material";
+import { useState } from "react";
+import { FormControl, InputLabel, Select, MenuItem, Input, Button } from "@mui/material";
 import "./styles/Glassmorphism.css";
 import "./styles/formsStyles.css";
 
 export default function InputForm() {
-    var inputCoin = 0;
+    const [amount, setAmount] = useState("");
+    const [inputCoin, setInputCoin] = useState("ARS");
+
     var result = 0;
-    const handleInputCoin = () => {
-        console.log("handleInputCoin");
+    const handleInputCoin = (event) => {
+        setInputCoin(event.target.value);
     }
+    const handleInputAmount = (event) => {
+        setAmount(event.target.value);
+    }
+    const sendPackage = () => {
+        console.log(inputCoin);
+        console.log(amount);
+    }
+    
     return(
         <div className="form-input Glassmorphism-2">
             <p className="header-text">INPUT A COIN</p>
@@ -21,12 +32,24 @@ export default function InputForm() {
                         label="Select Coin"
                         onChange={handleInputCoin}
                     >
-                        <MenuItem value="ARS">Peso argentino</MenuItem>
-                        <MenuItem value="DLBLUE">Dolar Blue</MenuItem>
-                        <MenuItem value="DLOFICIAL">Dolar Oficial</MenuItem>
-                        <MenuItem value="EUR">Euro</MenuItem>
+                        <MenuItem value={"ARS"}>Peso argentino</MenuItem>
+                        <MenuItem value={"DLBLUE"}>Dolar Blue</MenuItem>
+                        <MenuItem value={"DLOFICIAL"}>Dolar Oficial</MenuItem>
+                        <MenuItem value={"EUR"}>Euro</MenuItem>
                     </Select>
                 </FormControl>
+
+                <FormControl fullWidth>
+                    <InputLabel id="input-amount-label">Specify amount</InputLabel>
+                    <Input 
+                    required={true}
+                    labelId="input-amount-label"
+                    id='input-amount'
+                    value={amount}
+                    onChange={handleInputAmount}
+                    />
+                </FormControl>
+                <Button variant="contained" color="primary" onClick={sendPackage}/>
             </div>
             <div className="result-container">
                 <p className="result-value">{result}</p>
