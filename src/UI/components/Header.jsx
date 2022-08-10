f63e2949008cc18a32ab0321589ca44055ba12e7
GermanDobler <german.crack@hotmail.com>
2022-08-10 09:53:49 -0300

    Fix: Historial *Historial

--- src/UI/components/Header.jsx
+++ src/UI/components/Header.jsx
@@ -5,7 +5,7 @@ import "./styles/Glassmorphism.css";
 export default function Header() {
     return (
         <Fragment>
-            <Canvas camera={ { fov: 5, near: 0.1, far: 1, position: [-20, 0, 20] } }>
+            <Canvas camera={ { fov: 5, near: 0.1, far: 1000, position: [-20, 0, 20] } }>
                 <directionalLight intensity={3.5} position={[-10, 0, 0]} color="white" />
                 {/* <directionalLight intensity={1} position={[-10, 0, 25]} color="#05ffee" /> */}
                 <Logo />
